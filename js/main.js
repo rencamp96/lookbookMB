260362d42db4d09a6ac03196d978f599b01fc5e5
renatacamp <rcampuzanoc@centro.edu.mx>
2020-04-20 14:06:51 -0500

    Ventanas cerrables

--- js/main.js
+++ js/main.js
@@ -5,9 +5,7 @@ var imgHeightMedium = (imgWidthMedium / 864) * 1296;
 var imgWidthMovil = window.innerWidth / 100 * 55;
 var imgHeightMovil = (imgWidthMovil / 864) * 1296;
 
-function preload() {}
-
-function setup() {
+function init() {
 	// --------- Center Landing Page Horizontally -----------
 	var contentHeight = document.getElementById("cuadroRojo1").offsetHeight;
 	var totalHeight = document.documentElement.scrollHeight;
@@ -31,24 +29,10 @@ function myFunction(){
 		document.getElementById('backlight').style.display = 'inline-block';
 };
 
-function windowResized() {
+window.addEventListener("load", () => {
+	init();
+});
 
-	// --------- Center Landing Page Horizontally -----------
-
-	var totalHeight = document.documentElement.scrollHeight;
-	var contentHeight = document.getElementById("cuadroRojo1").offsetHeight;
-
-	// --------- Center Content Media Queries -----------
-
-	if (window.innerWidth >= 1061) {
-		margin = (totalHeight - (contentHeight / 5 * 6.5)) / 2;
-		document.getElementById('center').style.top = margin + 'px';
-	}
-	if (window.innerWidth <= 1060 && window.innerWidth >= 600) {
-		marginMedium = (totalHeight - (contentHeight / 5 * 9)) / 2;
-		document.getElementById('center').style.top = marginMedium + 'px';
-	}
-	if (window.innerWidth <= 600) {
-		marginMovil = (totalHeight - (contentHeight / 5 * 12)) / 2;
-	}
-}
+window.addEventListener('resize', () => {
+	init();
+}) 
